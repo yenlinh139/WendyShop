2891687cf8abbd9ce8a2451616d9f998d942e9af
yenlinh139 <160827968+yenlinh139@users.noreply.github.com>
2024-04-01 13:28:41 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import RouterCustom from './router';
-import './style/style.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import RouterCustom from "./router";
+import "./style/style.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <RouterCustom />
   </BrowserRouter>
 );
-
