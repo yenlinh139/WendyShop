954d08be3dff1bf083eeb65edbbc7f7592b0a149
yenlinh139 <160827968+yenlinh139@users.noreply.github.com>
2024-04-05 21:58:38 +0700

    update source code

--- src/pages/users/theme/header/index.js
+++ src/pages/users/theme/header/index.js
@@ -1,4 +1,4 @@
-import { memo, useState } from "react";
+import { memo, useState, useRef, useEffect } from "react";
 import "./style.scss";
 import {
   BsBellFill,
@@ -11,11 +11,48 @@ import {
   AiOutlineShoppingCart,
   AiOutlineHeart,
   AiOutlineUser,
+  AiOutlineMenu,
 } from "react-icons/ai";
 import { formaster } from "utils/formater";
 import { ROUTERS } from "utils/router";
 
 const Header = () => {
+  const [showLogin, setShowLogin] = useState(false); // State để điều khiển hiển thị khung đăng nhập
+  const [username, setUsername] = useState(""); // State lưu trữ tên người dùng
+  const [password, setPassword] = useState(""); // State lưu trữ mật khẩu
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const sidebarRef = useRef(null);
+
+  const toggleUser = () => {
+    setShowLogin(!showLogin);
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    // Xử lý đăng nhập ở đây
+    console.log(
+      "Đăng nhập với tên người dùng:",
+      username,
+      "và mật khẩu:",
+      password
+    );
+  };
+
+  const handleOverlayClick = (event) => {
+    // Kiểm tra xem sự kiện click có xuất phát từ phần tử login hay không
+    if (!event.target.closest(".login")) {
+      // Nếu không, đóng khung đăng nhập
+      setShowLogin(false);
+    }
+  };
+
   const [menus] = useState([
     {
       name: "Trang chủ",
@@ -53,6 +90,25 @@ const Header = () => {
       path: ROUTERS.USER.PROFILE,
     },
   ]);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        closeMenu();
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
 
   return (
     <>
@@ -96,7 +152,7 @@ const Header = () => {
       <div id="menu">
         <div className="container">
           <div className="row">
-            <div className="col-3">
+            <div className="col-6 col-lg-3 col-xl-3">
               <div className="header-logo">
                 <h1>
                   {" "}
@@ -104,47 +160,99 @@ const Header = () => {
                 </h1>
               </div>
             </div>
-            <div className="col-6">
-              <div className="header-menu">
-                <ul>
-                  {menus?.map((menu, menuKey) => (
-                    <li key={menuKey} className={menuKey === 0 ? "active" : ""}>
-                      <Link to={menu?.path}>{menu?.name}</Link>
-                      {menu.child && (
-                        <ul className="header-menu-dropdown">
-                          {menu.child.map((childItem, childKey) => (
-                            <li key={`${menuKey}-${childKey}`}>
-                              <Link to={childItem?.path}>
-                                {childItem?.name}
-                              </Link>
-                            </li>
-                          ))}
-                        </ul>
-                      )}
-                    </li>
-                  ))}
-                </ul>
+            <div className="col-6 col-lg-9 col-xl-9">
+              <div className="menuTop  d-lg-none d-xl-none">
+                <button onClick={toggleMenu}>
+                  <AiOutlineMenu />
+                </button>
               </div>
-            </div>
-            <div className="col-3">
-              <div className="header-cart">
-                <ul>
-                  <li>
-                    <Link to={""}>
-                      <AiOutlineUser />
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to={""}>
-                      <AiOutlineShoppingCart /> <span>5</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to={""}>
-                      <AiOutlineHeart /> <span>5</span>
-                    </Link>
-                  </li>
-                </ul>
+              <div
+                ref={sidebarRef}
+                className={`menu ${isMenuOpen ? "open" : ""}`}
+              >
+                <div className="row ">
+                  <div className="col-lg-8 col-xl-8">
+                    <div className="header-menu">
+                      <ul>
+                        {menus?.map((menu, menuKey) => (
+                          <li
+                            key={menuKey}
+                            className={menuKey === 0 ? "active" : ""}
+                          >
+                            <Link to={menu?.path}>{menu?.name}</Link>
+                            {menu.child && (
+                              <ul className="header-menu-dropdown">
+                                {menu.child.map((childItem, childKey) => (
+                                  <li key={`${menuKey}-${childKey}`}>
+                                    <Link to={childItem?.path}>
+                                      {childItem?.name}
+                                    </Link>
+                                  </li>
+                                ))}
+                              </ul>
+                            )}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-xl-4 ">
+                    <div className="header-cart">
+                      <ul>
+                        {/* Icon người dùng */}
+                        <li onClick={toggleUser}>
+                          <AiOutlineUser />
+                        </li>
+
+                        {/* Overlay */}
+                        {showLogin && (
+                          <div
+                            className="login-overlay"
+                            onClick={handleOverlayClick}
+                          ></div>
+                        )}
+
+                        {/* Khung đăng nhập */}
+                        {showLogin && (
+                          <div className="login">
+                            <h2>Đăng nhập</h2>
+                            <form>
+                              <div>
+                                <label>Tên người dùng:</label>
+                                <input
+                                  type="text"
+                                  value={username}
+                                  onChange={handleUsernameChange}
+                                />
+                              </div>
+                              <div>
+                                <label>Mật khẩu:</label>
+                                <input
+                                  type="password"
+                                  value={password}
+                                  onChange={handlePasswordChange}
+                                />
+                              </div>
+                              <button type="button" onClick={handleLogin}>
+                                Đăng nhập
+                              </button>
+                            </form>
+                          </div>
+                        )}
+                        <li>
+                          <Link to={""}>
+                            <AiOutlineShoppingCart /> <span>5</span>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to={""}>
+                            <AiOutlineHeart /> <span>5</span>
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
